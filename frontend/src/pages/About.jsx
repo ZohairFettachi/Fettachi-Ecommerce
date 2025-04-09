8c64ab74846cdb88ea7e99708ed364496aea6351
Zohair Fettachi <zohairfettachi.info@gmail.com>
2025-04-09 14:06:48 +0200

    Initial commit or update project

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsletterBox from '../components/NewsletterBox'
+
+const About = () => {
+  return (
+    <div>
+
+      <div className='text-2xl text-center pt-8 border-t'>
+          <Title text1={'ABOUT'} text2={'US'} />
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+          <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
+          <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+              <p>Northwearwas created from a passion for innovation and a vision to transform online shopping. Our journey started with a simple goal: to offer a seamless platform where customers can effortlessly discover, explore, and shop for a variety of products from the comfort of their homes.</p>
+              <p>Since day one, we've been dedicated to curating a diverse selection of high-quality items that suit every style and need. From fashion and beauty to electronics and home essentials, our collection features carefully sourced products from trusted brands and suppliers.</p>
+              <b className='text-gray-800'>Our Mission</b>
+              <p>Our mission At Northwearis to empower customers with choice, convenience, and confidence. We strive to deliver a seamless shopping experience that goes beyond expectations, from browsing and ordering to fast, reliable delivery and exceptional support.</p>
+          </div>
+      </div>
+
+      <div className=' text-xl py-4'>
+          <Title text1={'WHY'} text2={'CHOOSE US'} />
+      </div>
+
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Quality Assurance:</b>
+            <p className=' text-gray-600'>We meticulously select and vet each product to ensure it meets our highest quality standards and exceeds customer expectations.</p>
+          </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Convenience:</b>
+            <p className=' text-gray-600'>With our user-friendly interface and hassle-free ordering process, shopping is now quicker, simpler, and more enjoyable than ever. </p>
+          </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Exceptional Customer Service:</b>
+            <p className=' text-gray-600'>Our team of dedicated professionals is here to assist you the way, ensuring your satisfaction is our top priority.</p>
+          </div>
+      </div>
+
+      <NewsletterBox/>
+      
+    </div>
+  )
+}
+
+export default About
